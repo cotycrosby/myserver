7ce2fff36dde77158a849222b088f738818fec35
unknown <coty.crosby@gmail.com>
2020-08-29 00:33:18 -0400

    Including header

--- index.php
+++ index.php
@@ -6,10 +6,16 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
-	<h1>Helooo</h1>
+ <?php require_once('header.php'); ?>
+
+ <div class="container">
+ <h1>Helooo</h1>
 	<p>Update</p>
 	<p>
 		Heres a <a href="newfeature.php">LINK</a> to the newest feature!
 	</p>
+ </div>
+	
+	
 </body>
 </html>
